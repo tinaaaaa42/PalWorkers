7c685cdaf8411601ef721f3a692462e16258cf70
Dale Young <daleyoung4242@gmail.com>
2024-05-16 22:44:56 +0800

    Fix the package typo in CorsConfig

--- backend/src/main/java/com/palworker/backend/CorsConfig.java
+++ backend/src/main/java/com/palworker/backend/CorsConfig.java
@@ -1,4 +1,4 @@
-package com.young.bookstorebackend;
+package com.palworker.backend;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;

4e23926a648a9fdf7be016e37462500cc890f058
aobuaoye <1169652042@sjtu.edu.cn>
2024-05-25 19:49:37 +0800

    team

--- backend_JPA/demo/src/main/java/com/example/demo/controller/teamController.java
+++ backend_JPA/demo/src/main/java/com/example/demo/controller/teamController.java
@@ -2,7 +2,9 @@ package com.example.demo.controller;
 
 import com.example.demo.entity.Team;
 import com.example.demo.entity.TeamMember;
+import com.example.demo.entity.User;
 import com.example.demo.service.teamService;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,15 +18,18 @@ public class teamController {
     @Autowired
     private teamService teamService;
 
-    @GetMapping(value = "api/tasks/team")
-    public Team getTeam() {
-        int teamId = 1;
+    @GetMapping(value = "api/tasks/team/test")
+    public Team getTeamById(@RequestBody int teamId) {
         return teamService.findTeamById(teamId);
     }
 
-    @GetMapping(value = "api/tasks/team/test")
-    public List<TeamMember> getTeamsByUserId() {
-        int userId = 1;
+    @GetMapping(value = "api/tasks/team")
+    public List<TeamMember> getTeamsByUserId(HttpSession session) {
+        User user = (User) session.getAttribute("user");
+        Integer userId = user.getId();
+        if (userId == null) {
+            throw new RuntimeException("User not logged in");
+        }
         return teamService.findTeamsByUserId(userId);
     }
 }

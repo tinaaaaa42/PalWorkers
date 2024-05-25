21b41200f75d7326e7596b17762dd09b0ea956ae
samllYYang <2279507332@qq.com>
2024-05-25 15:28:48 +0800

    nav

--- frontend/src/component/Navv.js
+++ frontend/src/component/Navv.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { AppstoreOutlined, MailOutlined, SettingOutlined } from '@ant-design/icons';
 import { Menu } from 'antd';
-
 const items = [
   {
     key: 'sub1',
@@ -93,18 +92,17 @@ const items = [
     ],
   },
 ];
-
 const onClick = (e) => {
   console.log('click', e);
 };
-
 const Navbar = () => (
   <Menu
     onClick={onClick}
-    style={{ width: 256 }}
-    mode="vertical"
+    style={{
+      width: 256,
+    }}
+    mode="inline"
     items={items}
   />
 );
-
 export default Navbar;

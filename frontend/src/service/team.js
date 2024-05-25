6d27187bab3b1ac1d95adb31f760f5d2b21c5ce8
aobuaoye <1169652042@sjtu.edu.cn>
2024-05-25 23:30:30 +0800

    api done

--- frontend/src/service/team.js
+++ frontend/src/service/team.js
@@ -1,10 +1,25 @@
-import {getJson, PREFIX} from "./common";
+import {getJson, PREFIX, post} from "./common";
 
 export async function get_team() {
     const url = `${PREFIX}/tasks/team`;
     let team;
     try {
         team = await getJson(url);
+    } catch (e) {
+        console.log(e);
+        team = {
+            total: 0,
+            items: []
+        };
+    }
+    return team;
+}
+
+export async function create_team(teamName) {
+    const url = `${PREFIX}/tasks/team`;
+    let team;
+    try {
+        team = await post(url,teamName)
     } catch (e) {
         console.log(e);
         team = {

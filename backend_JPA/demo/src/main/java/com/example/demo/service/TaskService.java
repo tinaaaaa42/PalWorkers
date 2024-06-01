bd476f3073ce614e5b113a2a68b8fa6977e75010
Dale Young <daleyoung4242@gmail.com>
2024-06-01 13:27:25 +0800

    Get back TaskService

---
+++ backend_JPA/demo/src/main/java/com/example/demo/service/TaskService.java
@@ -0,0 +1,8 @@
+package com.example.demo.service;
+
+import com.example.demo.DTO.TaskDto;
+import com.example.demo.entity.Task;
+
+public interface TaskService {
+    Task createTask(TaskDto taskDto);
+}
